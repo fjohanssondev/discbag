1343505c866e375f4688ca08f12478da6ad6aa45
fjohanssonnn <fjohansson.creative@gmail.com>
2023-04-23 18:37:17 +0200

    Added procedure to fetch a single user based on their userId

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -6,6 +6,17 @@ import {
 } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
+
+  getById: protectedProcedure
+    .input(z.string())
+    .query(({ ctx, input }) => {
+      return ctx.prisma.user.findFirst({
+        where: {
+          id: input
+        }
+      })
+    }),
+
   isUserAdmin: protectedProcedure
     .query(({ ctx }) => {
       return ctx.prisma.user.findUnique({
