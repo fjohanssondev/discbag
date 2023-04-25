da9bc7d1177e7d5b811894d73046bfb7e74a43ec
fjohanssonnn <fjohansson.creative@gmail.com>
2023-04-25 23:45:18 +0200

    Updated the info of each disc. Also updated the "Add to bag" button so the user can see their bags and add the disc to any of it

--- src/pages/discs/product/[id].tsx
+++ src/pages/discs/product/[id].tsx
@@ -1,6 +1,10 @@
-import Link from "next/link"
 import { useRouter } from "next/router"
+import { useState } from "react"
+import { Field, Form } from "houseform";
+
+import Modal from "~/components/Modal"
 import { api } from "~/utils/api"
+import { discType } from "utils/discType";
 
 const DiscPage = () => {
 
@@ -11,13 +15,79 @@ const DiscPage = () => {
     id: id as string
   })
 
+  const { data: bags } = api.bag.getAllMyBags.useQuery()
+
+  const { mutate } = api.bag.addDiscToBag.useMutation()
+
+  const addToBag = async (bagId: string) => {
+    mutate({
+      bagId: bagId,
+      discId: id as string
+    })
+
+    await router.push(`/bags/${bagId}`)
+  }
+
   return (
     <section>
       <div className="md:container md:mx-auto px-4 mt-12">
-        <div className="flex justify-between items-center">
-          <h1 className="text-2xl font-semibold">{disc?.name}</h1>
-          <Link href="/bag" className="flex justify-center items-center bg-indigo-500 hover:bg-indigo-400 text-sm text-indigo-50 px-4 py-2 rounded">Add to bag</Link>
-        </div>
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <h1 className="text-4xl font-bold">{disc?.name}</h1>
+            <p className="text-sm text-gray-500 pt-1">{discType(disc?.type as string)} | {disc?.manufacturer}</p>
+            <p>{disc?.description}</p>
+            <div className="mt-8">
+              <ul className="flex items-center gap-2">
+                <li className="text-sm">Speed: {disc?.speed}</li>
+                <li className="text-sm">Glide: {disc?.glide}</li>
+                <li className="text-sm">Turn: {disc?.turn}</li>
+                <li className="text-sm">Fade: {disc?.fade}</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <Modal buttonText="Add to bag">
+              { /* TODO: Refactor later */}
+              <Form onSubmit={({ bag }: { bag: string }) => void addToBag(bag)}>
+                {({ submit }: { submit: () => void }) => (
+                  <div>
+                    <Field<string>
+                      name="bag"
+                    >
+                      {({ value, setValue, onBlur, errors }) => {
+                        return (
+                          <div className="flex flex-col mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="disc">
+                              What bag do you want to add this disc to?
+                            </label>
+                            <select
+                              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                              name="disc"
+                              id="disc"
+                              value={value}
+                              onChange={(e) => setValue(e.target.value)}
+                              onBlur={onBlur}
+                            >
+                              <optgroup label="My bags">
+                                {bags?.map((bag) => (
+                                  <option key={bag.id} value={bag.id}>{bag.name}</option>
+                                ))}
+                              </optgroup>
+                            </select>
+                            {errors.map((error) => (
+                              <p key={error}>{error}</p>
+                            ))}
+                          </div>
+                        );
+                      }}
+                    </Field>
+                    <button className="flex w-full justify-center items-center bg-indigo-500 hover:bg-indigo-400 text-sm text-indigo-50 px-4 py-2 rounded" onClick={submit}>Add to bag</button>
+                  </div>
+                )}
+              </Form>
+            </Modal>
+          </div>
+          </div>
       </div>
     </section>
   )
