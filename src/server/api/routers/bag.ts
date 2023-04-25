b0b28cb8e533a1cf9ca29a94e4d112cd68e69492
fjohanssonnn <fjohansson.creative@gmail.com>
2023-04-25 23:44:21 +0200

    Added procedure to fetch all bags by the signed in user. Also a procedure to mutate the bag by adding a disc

--- src/server/api/routers/bag.ts
+++ src/server/api/routers/bag.ts
@@ -20,6 +20,32 @@ export const bagRouter = createTRPCRouter({
       });
     }),
 
+  getAllMyBags: protectedProcedure
+    .query(({ ctx }) => {
+      return ctx.prisma.bag.findMany({
+        where: {
+          userId: ctx.session.user.id,
+        }
+      });
+    }),
+
+  addDiscToBag: protectedProcedure
+    .input((z.object({ bagId: z.string(), discId: z.string() })))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.bag.update({
+        where: {
+          id: input.bagId,
+        },
+        data: {
+          discs: {
+            connect: {
+              id: input.discId,
+            },
+          },
+        },
+      });
+    }),
+
   numberOfBags: protectedProcedure
     .query(({ ctx }) => {
       return ctx.prisma.bag.count({
