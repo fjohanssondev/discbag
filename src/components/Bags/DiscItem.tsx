9df74b7db3b017fdc69dfb020c7033eea46f7ed2
fjohanssonnn <fjohansson.creative@gmail.com>
2023-04-25 23:45:51 +0200

    UI

--- src/components/Bags/DiscItem.tsx
+++ src/components/Bags/DiscItem.tsx
@@ -15,7 +15,7 @@ export interface DiscProps {
 const DiscItem: React.FC<DiscProps> = ({ name, speed, glide, turn, fade, manufacturer, type }) => {
   return (
     <article className='flex items-center border border-slate-300 rounded-sm p-4 mb-4'>
-      <h4 className='font-bold'>{name}</h4>
+      <h4 className='font-bold min-w-[12rem]'>{name}</h4>
       <div className='ml-8'>
         <ul className='flex items-center gap-2'>
           <li className='text-sm'>Speed: {speed}</li>
