4fe38ac2951b4c1f3f501ac7803f4523e74d6a1f
fjohanssonnn <fjohansson.creative@gmail.com>
2023-04-29 21:12:42 +0200

    Updated the all-procedure to only fetch an x amount of discs. Enabling pagination

--- src/server/api/routers/disc.ts
+++ src/server/api/routers/disc.ts
@@ -10,8 +10,33 @@ const DiscType = ["PUTTER", "MIDRANGE", "FAIRWAY_DRIVER", "DISTANCE_DRIVER"] as 
 
 export const discRouter = createTRPCRouter({
   all: publicProcedure
-    .query(({ ctx }) => {
-      return ctx.prisma.disc.findMany();
+    .input(z.object({
+      limit: z.number(),
+      cursor: z.string().nullish(),
+      skip: z.number().optional(),
+    }))
+    .query(async ({ ctx, input }) => {
+      const { limit, cursor, skip } = input;
+
+      const discs = await ctx.prisma.disc.findMany({
+        take: limit + 1,
+        skip,
+        cursor: cursor ? { id: cursor } : undefined,
+        orderBy: {
+          name: "asc",
+        },
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (discs.length > limit) {
+        const nextItem = discs.pop();
+        nextCursor = nextItem?.id;
+      }
+
+      return {
+        discs,
+        nextCursor,
+      };
     }),
 
   getById: publicProcedure
@@ -25,18 +50,20 @@ export const discRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string(), description: z.string(), manufacturer: z.string(), type: z.enum(DiscType), speed: z.number(), glide: z.number(), turn: z.number(), fade: z.number() }))
+    .input(z.object({ name: z.string(), description: z.optional(z.string()), manufacturer: z.string(), image: z.optional(z.string()), stability: z.string(), type: z.enum(DiscType), speed: z.number(), glide: z.number(), turn: z.number(), fade: z.number() }))
     .mutation(({ input, ctx }) => {
       return ctx.prisma.disc.create({
         data: {
           name: input.name,
           description: input.description,
           manufacturer: input.manufacturer,
+          image: input.image,
           type: input.type,
           speed: input.speed,
           glide: input.glide,
           turn: input.turn,
           fade: input.fade,
+          stability: input.stability,
         },
       });
     }),
